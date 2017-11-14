afecf6f606b5391c2cf4abc2a2fae8b371294f14
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-15 00:15:13 +0700

    Rebuild all new recyclerview instead of listview

--- app/src/main/java/prokedex/com/xtreme/prokedex/PokemonListAdapter.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/PokemonListAdapter.java
@@ -1,6 +1,7 @@
 package prokedex.com.xtreme.prokedex;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,50 +15,53 @@ import java.util.ArrayList;
  * Created by KanTLovE on 11/13/2017.
  */
 
-public class PokemonListAdapter extends BaseAdapter {
+public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.MyViewHolder> {
 
-    Context mContext;
-    ArrayList<Pokemon> pokemon;
+    private final LayoutInflater inflater;
+    private ArrayList<Pokemon> pokemons;
 
-    public PokemonListAdapter(Context context, ArrayList<Pokemon> pokemon) {
-        this.mContext= context;
-        this.pokemon = pokemon;
+
+    public PokemonListAdapter(Context context, ArrayList<Pokemon> pokemons){
+        inflater = LayoutInflater.from(context);
+        this.pokemons = pokemons;
     }
 
-    public int getCount() {
-        return pokemon.size();
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View itemView = inflater.inflate(R.layout.pokemon_lists, viewGroup, false);
+        return new MyViewHolder(itemView);
     }
 
-    public Object getItem(int position) {
-        return null;
+    @Override
+    public void onBindViewHolder(MyViewHolder viewHolder, int position) {
+        viewHolder.name.setText(pokemons.get(position).getName());
+        viewHolder.nameJap.setText(pokemons.get(position).getNameJap());
+        viewHolder.element1.setText(pokemons.get(position).getElement1());
+        viewHolder.element2.setText(pokemons.get(position).getElement2());
+        viewHolder.sprite.setBackgroundResource(pokemons.get(position).getResId());
+
     }
 
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount(){
+        return 9;
     }
 
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater mInflater =
-                (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        if(view == null)
-            view = mInflater.inflate(R.layout.pokemon_lists, parent, false);
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textView1 = (TextView)view.findViewById(R.id.textView1);
-        textView1.setText(pokemon.get(position).getName());
+        protected TextView name;
+        protected TextView nameJap;
+        protected TextView element1;
+        protected TextView element2;
+        protected ImageView sprite;
 
-        TextView textView2 = (TextView)view.findViewById(R.id.textView2);
-        textView2.setText(pokemon.get(position).getNameJap());
-
-        TextView textView3 = (TextView)view.findViewById(R.id.textView3);
-        textView3.setText(pokemon.get(position).getElement1());
-
-        TextView textView4 = (TextView)view.findViewById(R.id.textView4);
-        textView4.setText(pokemon.get(position).getElement2());
-
-        ImageView imageView = (ImageView)view.findViewById(R.id.imageView1);
-        imageView.setBackgroundResource(pokemon.get(position).getResId());
-
-        return view;
+        private MyViewHolder(View v) {
+            super(v);
+            this.name = (TextView) v.findViewById(R.id.textView1);
+            this.nameJap = (TextView) v.findViewById(R.id.textView2);
+            this.element1 = (TextView) v.findViewById(R.id.textView3);
+            this.element2 = (TextView) v.findViewById(R.id.textView4);
+            this.sprite = (ImageView) v.findViewById(R.id.imageView1);
+        }
     }
 }
