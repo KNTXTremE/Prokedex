2bd8f766c0cb836b6d49266644913e670aa83a03
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-14 01:14:57 +0700

    Add Pokemon Description activity

---
+++ app/src/main/java/prokedex/com/xtreme/prokedex/PokeDescActivity.java
@@ -0,0 +1,41 @@
+package prokedex.com.xtreme.prokedex;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PokeDescActivity extends AppCompatActivity {
+
+    private static final String TAG = "PokeDescActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedesc);
+        Log.d(TAG, "onCreate: started.");
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: check for incoming intents");
+        if(getIntent().hasExtra("pokemon_name") && getIntent().hasExtra("image")){
+            Log.d(TAG, "getIncomingIntent: found intents!");
+            String pokemon_name = getIntent().getStringExtra("pokemon_name");
+            String imageId = getIntent().getStringExtra("imageId");
+            setItems(pokemon_name, imageId);
+
+        }
+    }
+
+    private void setItems(String pokemonName, String imageId){
+        Log.d(TAG, "setItems: settings items to the widgets");
+        TextView name = findViewById(R.id.pokemon_name_desc);
+        name.setText(pokemonName);
+
+        ImageView image = findViewById(R.id.image_desc);
+        image.setBackgroundResource(Integer.parseInt(imageId));
+    }
+}
