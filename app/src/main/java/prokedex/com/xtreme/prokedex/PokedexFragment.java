333fd403f58b4416bb449e8f83e2c49796d893be
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-23 00:54:37 +0700

    Cleanup fragment to reimplement

--- app/src/main/java/prokedex/com/xtreme/prokedex/PokedexFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/PokedexFragment.java
@@ -1,139 +1,16 @@
 package prokedex.com.xtreme.prokedex;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PokedexFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PokedexFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PokedexFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-//    RecyclerView recyclerPokemonView;
-//    ArrayList<Pokemon> allPokemon;
-
-    public PokedexFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PokedexFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PokedexFragment newInstance(String param1, String param2) {
-        PokedexFragment fragment = new PokedexFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-//        AllItems.addElements();
-//        show_list();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(String  title) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(title);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-//    private ArrayList<Pokemon> getData(){
-//        ArrayList<Pokemon> allPokemon = new ArrayList<>();
-//        for(int i = 0; i < AllItems.getResIds().length; i++){
-//            Pokemon pkm = new Pokemon(AllItems.getPokemonName(i), AllItems.getPokemonNameJap(i), AllItems.getResId(i),AllItems.getElement1(i), AllItems.getElement2(i));
-//            allPokemon.add(pkm);
-//        }
-//        return allPokemon;
-//    }
-
-//    private void show_list() {
-//        recyclerPokemonView = (RecyclerView) MainActivity.findViewById((R.id.pokemon_list_recycle));
-//        recyclerPokemonView.setHasFixedSize(true);
-//        recyclerPokemonView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-//        recyclerPokemonView.setItemAnimator(new DefaultItemAnimator());
-//        allPokemon = getData();
-//        PokemonListAdapter adapter = new PokemonListAdapter(this, allPokemon);
-//
-//        recyclerPokemonView.setAdapter(adapter);
-//    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(String title);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_main, null);
     }
 }
