8c017588f9242ea1fd468a40a27c0d7d677173e1
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-23 14:05:03 +0700

    Remove content_main xml and change to fragment_pokedex xml

--- app/src/main/java/prokedex/com/xtreme/prokedex/PokedexFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/PokedexFragment.java
@@ -11,6 +11,6 @@ public class PokedexFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_main, null);
+        return inflater.inflate(R.layout.fragment_pokedex, null);
     }
 }
