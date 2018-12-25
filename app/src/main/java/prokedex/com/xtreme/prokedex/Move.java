8c787bbac73f9fcb02792042ac5d66bde87fa420
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-25 19:37:46 +0700

    Add move object

---
+++ app/src/main/java/prokedex/com/xtreme/prokedex/Move.java
@@ -0,0 +1,37 @@
+package prokedex.com.xtreme.prokedex;
+
+public class Move {
+    private String name;
+    private int type;
+    private int category;
+    private String power;
+    private String accuracy;
+
+    public Move(String name, int type, int category, String power, String accuracy){
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.power = power;
+        this.accuracy = accuracy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public String getAccuracy() {
+        return accuracy;
+    }
+}
