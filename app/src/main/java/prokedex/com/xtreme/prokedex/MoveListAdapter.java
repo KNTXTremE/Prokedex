e372168ff0d0611e2b09a74e2c16fcd315df5dd3
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-25 19:38:16 +0700

    Add move recyclerview to movedex fragment

---
+++ app/src/main/java/prokedex/com/xtreme/prokedex/MoveListAdapter.java
@@ -0,0 +1,86 @@
+package prokedex.com.xtreme.prokedex;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MoveListAdapter extends RecyclerView.Adapter<MoveListAdapter.MyViewHolder> {
+    private final LayoutInflater inflater;
+    private ArrayList<Move> moves;
+    private Context context;
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    public MoveListAdapter(Context context, ArrayList<Move> moves){
+        inflater = LayoutInflater.from(context);
+        this.moves = moves;
+        this.context = context;
+
+    }
+
+    @Override
+    public MoveListAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View itemView = inflater.inflate(R.layout.move_lists, viewGroup, false);
+        MoveListAdapter.MyViewHolder viewHolder = new MoveListAdapter.MyViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MoveListAdapter.MyViewHolder viewHolder, final int position) {
+        viewHolder.moveName.setText(moves.get(position).getName());
+        viewHolder.moveType.setBackgroundResource(AllItems.getElements().get(moves.get(position).getType()));
+        viewHolder.moveCategory.setText(AllItems.getMoveCategory()[moves.get(position).getCategory()]);
+        viewHolder.movePower.setText(moves.get(position).getPower());
+        viewHolder.moveAccuracy.setText(moves.get(position).getAccuracy());
+
+//        viewHolder.moveCardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "onClick: clicked on: " + moves.get(position));
+//
+//                Intent intent = new Intent(context, MoveDescActivity.class);
+//                intent.putExtra("move_name", moves.get(position).getName());
+//                intent.putExtra("move_type", moves.get(position).getType());
+//                context.startActivity(intent);
+//            }
+//        });
+    }
+
+    public int getItemCount(){
+        return moves.size();
+    }
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        protected CardView moveCardView;
+        protected TextView moveName;
+        protected ImageView moveType;
+        protected TextView moveCategory;
+        protected TextView movePower;
+        protected TextView moveAccuracy;
+
+        private MyViewHolder(View v) {
+            super(v);
+            this.moveCardView = v.findViewById(R.id.cardview_movedex);
+            this.moveName = v.findViewById(R.id.move_name);
+            this.moveType = v.findViewById(R.id.move_type);
+            this.moveCategory = v.findViewById(R.id.move_category);
+            this.movePower = v.findViewById(R.id.move_power);
+            this.moveAccuracy = v.findViewById(R.id.move_accuracy);
+        }
+
+
+    }
+}
--- app/src/main/java/prokedex/com/xtreme/prokedex/MovedexFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/MovedexFragment.java
@@ -3,15 +3,46 @@ package prokedex.com.xtreme.prokedex;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MovedexFragment extends Fragment {
+
+    private static final String TAG = "MovedexFragment";
+    RecyclerView recyclerMoveView;
+    ArrayList<Move> allMove;
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movedex, null);
+        View view = inflater.inflate(R.layout.fragment_movedex, null);
+        recyclerMoveView = view.findViewById((R.id.move_list_recycle));
+        recyclerMoveView.setHasFixedSize(true);
+        recyclerMoveView.setNestedScrollingEnabled(false);
+        recyclerMoveView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerMoveView.setItemAnimator(new DefaultItemAnimator());
+        allMove = getData();
+        MoveListAdapter adapter = new MoveListAdapter(getContext(), allMove);
+
+        recyclerMoveView.setAdapter(adapter);
+        return view;
+    }
+
+    private ArrayList<Move> getData() {
+        ArrayList<Move> allMove = new ArrayList<>();
+        for(int i = 1; i <= AllItems.getMoves().size(); i++){
+            Move move = new Move(AllItems.getMoves().get(i)[0], Integer.parseInt(AllItems.getMoves().get(i)[1]), Integer.parseInt(AllItems.getMoves().get(i)[2]), AllItems.getMoves().get(i)[3], AllItems.getMoves().get(i)[4]);
+            allMove.add(move);
+        }
+        return allMove;
     }
 }
