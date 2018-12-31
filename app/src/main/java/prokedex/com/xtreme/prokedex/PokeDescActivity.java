899a92454c32b2fe5c9ad69336fba5d447f0f9f4
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-31 14:11:21 +0700

    Add cardview elements from passed parameter from pokedex fragment

--- app/src/main/java/prokedex/com/xtreme/prokedex/PokeDescActivity.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/PokeDescActivity.java
@@ -21,22 +21,46 @@ public class PokeDescActivity extends AppCompatActivity {
 
     private void getIncomingIntent(){
         Log.d(TAG, "getIncomingIntent: check for incoming intents");
-        if(getIntent().hasExtra("pokemon_name") && getIntent().hasExtra("imageId")){
+        if(getIntent().hasExtra("pokemon_id") && getIntent().hasExtra("pokemon_isCaught")){
             Log.d(TAG, "getIncomingIntent: found intents!");
-            String pokemon_name = getIntent().getStringExtra("pokemon_name");
-            int imageId = getIntent().getIntExtra("imageId", 0);
-            setItems(pokemon_name, imageId);
+            Integer pokemon_id = Integer.parseInt(getIntent().getStringExtra("pokemon_id"));
+            boolean pokemon_isCaught = getIntent().getBooleanExtra("pokemon_isCaught", false);
+            setItems(pokemon_id, pokemon_isCaught);
 
         }
     }
 
-    private void setItems(String pokemonName, int imageId){
+    private void setItems(Integer pokemonId, boolean pokemon_isCaught){
         Log.d(TAG, "setItems: settings items to the widgets");
-        TextView name = findViewById(R.id.pokemon_name_desc);
-        name.setText(pokemonName);
+        String pokemonIdReal = "#" + String.format("%03d", pokemonId+1);
+        TextView pokedescId = findViewById(R.id.pokedesc_id);
+        pokedescId.setText(pokemonIdReal);
 
-        ImageView image = findViewById(R.id.image_desc);
-        image.setBackgroundResource(imageId);
+        TextView pokedescName = findViewById(R.id.pokedesc_name);
+        pokedescName.setText(AllItems.getPokemonName(pokemonId));
+
+        TextView pokedescNameJap = findViewById(R.id.pokedesc_name_jap);
+        pokedescNameJap.setText(AllItems.getPokemonNameJap(pokemonId));
+
+        ImageView pokedescImage = findViewById(R.id.pokedesc_sprite);
+        pokedescImage.setBackgroundResource(AllItems.getResId(pokemonId));
+
+        TextView pokedescElement1 = findViewById(R.id.pokedesc_element1);
+        pokedescElement1.setText(AllItems.getElements().get(AllItems.getElement1(pokemonId)));
+        pokedescElement1.setBackgroundColor(Color.parseColor(AllItems.getElementsColor(AllItems.getElement1(pokemonId))));
+
+        TextView pokedescElement2 = findViewById(R.id.pokedesc_element2);
+        pokedescElement2.setText(AllItems.getElements().get(AllItems.getElement2(pokemonId)));
+        pokedescElement2.setBackgroundColor(Color.parseColor(AllItems.getElementsColor(AllItems.getElement2(pokemonId))));
+
+        TextView pokedescIsCaught = findViewById(R.id.pokedesc_is_caught);
+        if(pokemon_isCaught){
+            pokedescIsCaught.setText("Caught");
+        }
+        else{
+            pokedescIsCaught.setText("NCaught");
+        }
+
         Toolbar pokedescToolBar = findViewById(R.id.pokedesc_toolbar);
         setSupportActionBar(pokedescToolBar);
         pokedescToolBar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
