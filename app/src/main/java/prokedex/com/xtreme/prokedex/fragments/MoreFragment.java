efd3794e8beb4abc05f78b4d3f0f27e171c78b5f
KNTXTremE <Kan.Horwang@gmail.com>
2019-01-04 02:18:45 +0700

    Add more fragment contents and fix layout overlap with bottomnavview

--- app/src/main/java/prokedex/com/xtreme/prokedex/fragments/MoreFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/fragments/MoreFragment.java
@@ -1,18 +1,34 @@
 package prokedex.com.xtreme.prokedex.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import prokedex.com.xtreme.prokedex.AboutActivity;
 import prokedex.com.xtreme.prokedex.R;
 
 public class MoreFragment extends Fragment {
+    private static final String TAG = "MoreFragment";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_more, null);
+        View view = inflater.inflate(R.layout.fragment_more, null);
+        CardView about = view.findViewById(R.id.more_about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Going to About activity");
+                Intent intent = new Intent(getActivity(), AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
