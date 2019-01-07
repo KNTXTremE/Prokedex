0f337e3e4979abb664a28d13d54d7df04bd09cb8
KNTXTremE <Kan.Horwang@gmail.com>
2019-01-07 23:22:59 +0700

    Add toolbar and arrange layout for all remaining menus

--- app/src/main/java/prokedex/com/xtreme/prokedex/AbilityDexActivity.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/AbilityDexActivity.java
@@ -1,13 +1,50 @@
 package prokedex.com.xtreme.prokedex;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AbilityDexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+
         setContentView(R.layout.activity_ability_dex);
+
+        Toolbar toolbar = findViewById(R.id.ability_dex_toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("AbilityDex");
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.setStatusBarColor(Color.BLACK);
+            }
+        } else {
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/prokedex/com/xtreme/prokedex/IVCalculatorActivity.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/IVCalculatorActivity.java
@@ -24,7 +24,13 @@ public class IVCalculatorActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_iv_calculator);
+
+        Toolbar toolbar = findViewById(R.id.iv_calculator_toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("IV Calculator");
+
         if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
             Window window = this.getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -32,11 +38,16 @@ public class IVCalculatorActivity extends AppCompatActivity {
                 window.setStatusBarColor(Color.BLACK);
             }
         } else {
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
-        Toolbar toolbar = findViewById(R.id.iv_calculator_toolbar);
-        setSupportActionBar(toolbar);
-        setTitle("IV Calculator");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
