6eda07e5fc9263e7a7d718306fe1dd6c773c7406
KNTXTremE <Kan.Horwang@gmail.com>
2019-01-15 01:09:22 +0700

    Set animation in others 2 fragments

--- app/src/main/java/prokedex/com/xtreme/prokedex/fragments/ItemdexFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/fragments/ItemdexFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import prokedex.com.xtreme.prokedex.R;
 
@@ -13,6 +15,14 @@ public class ItemdexFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_itemdex, null);
+        View view = inflater.inflate(R.layout.fragment_itemdex, null);
+        setAnimation(view);
+        return view;
+    }
+
+    private void setAnimation(View viewToAnimate)
+    {
+        Animation animation = AnimationUtils.loadAnimation(getContext(), android.R.anim.fade_in);
+        viewToAnimate.startAnimation(animation);
     }
 }
--- app/src/main/java/prokedex/com/xtreme/prokedex/fragments/MoreFragment.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/fragments/MoreFragment.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import prokedex.com.xtreme.prokedex.AbilityDexActivity;
@@ -109,6 +111,14 @@ public class MoreFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        setAnimation(view);
         return view;
+    }
+
+    private void setAnimation(View viewToAnimate)
+    {
+        Animation animation = AnimationUtils.loadAnimation(getContext(), android.R.anim.fade_in);
+        viewToAnimate.startAnimation(animation);
     }
 }
