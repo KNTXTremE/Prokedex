d28782e96906ae60e9b4dd2d48cea11975a5db52
KNTXTremE <Kan.Horwang@gmail.com>
2019-01-15 11:06:24 +0700

    Add GitHub link

--- app/src/main/java/prokedex/com/xtreme/prokedex/AboutActivity.java
+++ app/src/main/java/prokedex/com/xtreme/prokedex/AboutActivity.java
@@ -7,9 +7,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -26,7 +30,15 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle("About Prokedex");
 
+        TextView link = findViewById(R.id.link_to_github);
+        String linkText = "Follow me on <a href='https://github.com/KNTXTremE'>GitHub</a>.";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+        ImageView githubImage = findViewById(R.id.about_github_image);
+
         if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            githubImage.setBackgroundResource(R.drawable.ic_github_mark_light);
+
             toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
             Window window = this.getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -35,6 +47,8 @@ public class AboutActivity extends AppCompatActivity {
                 window.setStatusBarColor(Color.BLACK);
             }
         } else {
+            githubImage.setBackgroundResource(R.drawable.ic_github_mark_dark);
+
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         }
